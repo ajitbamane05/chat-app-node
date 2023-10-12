9590b035b7c15a886f21417b2fb30d3c391b1598
ajitbamane05 <ajitbamane05@gmail.com>
2023-10-12 20:05:26 +0530

    "test modifyed"

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors');
+const app = express()
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes.js');
 const chatRoutes = require('./routes/chatRoutes.js');
 const roomRoutes = require('./routes/roomRoutes.js');
-
-const app = express()
 app.use(express.json())
+const crosOptions = {
+    origin:'http://localhost:3001',
+    optionsSuccessStatus: 200
+}
+app.use(cors(crosOptions));
+
 app.get('/', (req, res) => {
     res.status(200).json({message:"Hello from backend"})
 })
--- tests/test_node_message_app.py
+++ tests/test_node_message_app.py
@@ -8,58 +8,58 @@ def test_can_call_endpoint():
     response = requests.get(ENDPOINT)
     assert response.status_code == 200
 
-# def test_user_login():
-#     payload={
-#     "username":"Steve",
-#     "password":"password"
-#     }
-#     response = requests.post(F"{ENDPOINT}api/login", json=payload)
-
-#     current_time = datetime.datetime.now()
-#     expires_at = current_time + datetime.timedelta(seconds=60*60*24)
-#     token = response.headers['authorization']
-#     token_data = {
-#         'issued at':current_time.strftime('%Y-%m-%d %H:%M:%S'),
-#         'expires at':expires_at.strftime('%Y-%m-%d %H:%M:%S'),
-#         'token':token
-#     }
-#     with open('user-token.json','w') as file:
-#         json.dump(token_data, file, indent=4)
-
-#     assert response.status_code == 200
-
-# def test_login_admin():
-#     payload={
-#     "username":"Admin",
-#     "password":"password"
-#     }
-#     response = requests.post(F"{ENDPOINT}api/login", json=payload)
-
-#     current_time = datetime.datetime.now()
-#     expires_at = current_time + datetime.timedelta(seconds=60*60*24)
-#     token = response.headers['authorization']
-#     token_data = {
-#         'issued at':current_time.strftime('%Y-%m-%d %H:%M:%S'),
-#         'expires at':expires_at.strftime('%Y-%m-%d %H:%M:%S'),
-#         'token':token
-#     }
-#     with open('admin-token.json','w') as file:
-#         json.dump(token_data, file, indent=4)
-
-#     assert response.status_code == 200
-
-
-def test_get_room_memberships():
-    payload = {
-    "userId":"86621119-8fc7-4695-b270-3d999d670821"
+def test_user_login():
+    payload={
+    "username":"Steve",
+    "password":"password"
     }
     with open('user-token.json') as infile:
         token = json.load(infile)
-    headers = {}
-    headers['authorization'] = token['token']
-    response = requests.post(F"{ENDPOINT}/api/room/getmembership",headers=headers, json=payload)
-    print(response.json())
-    assert response.status_code == 200
+    expiry=datetime.datetime.strptime(token['expires at'], '%Y-%m-%d %H:%M:%S')
+    if expiry > datetime.datetime.now():
+        assert True
+    else:
+        response = requests.post(F"{ENDPOINT}/api/login", json=payload)
+        current_time = datetime.datetime.now()
+        expires_at = current_time + datetime.timedelta(seconds=60*60*24)
+        token = response.headers['authorization']
+        token_data = {
+            'issued at':current_time.strftime('%Y-%m-%d %H:%M:%S'),
+            'expires at':expires_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'token':token
+        }
+        with open('user-token.json','w') as file:
+            json.dump(token_data, file, indent=4)
+            file.close()
+        assert response.status_code == 200
+
+def test_login_admin():
+    payload={
+    "username":"Admin",
+    "password":"password"
+    }
+    with open('admin-token.json') as infile:
+        token = json.load(infile)
+    expiry=datetime.datetime.strptime(token['expires at'], '%Y-%m-%d %H:%M:%S')
+    if expiry > datetime.datetime.now():
+        assert True
+    else:
+        response = requests.post(F"{ENDPOINT}/api/login", json=payload)
+
+        current_time = datetime.datetime.now()
+        expires_at = current_time + datetime.timedelta(seconds=60*60*24)
+        token = response.headers['authorization']
+        token_data = {
+            'issued at':current_time.strftime('%Y-%m-%d %H:%M:%S'),
+            'expires at':expires_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'token':token
+        }
+        with open('admin-token.json','w') as file:
+            json.dump(token_data, file, indent=4)
+            file.close()
+
+        assert response.status_code == 200
+
 
 def test_create_user_by_user():
     payload = {
@@ -72,10 +72,24 @@ def test_create_user_by_user():
         token = json.load(infile)
     headers = {}
     headers['authorization'] = token['token']
+    print(token['token'])
     response = requests.post(F"{ENDPOINT}/api/user/createuser",headers=headers, json=payload)
-
     print(response.json())
     assert response.status_code == 401
+
+def test_get_room_memberships_of_user():
+    payload = {
+        "userId":"86621119-8fc7-4695-b270-3d999d670821"
+    }
+    with open('user-token.json') as infile:
+        token = json.load(infile)
+    headers = {}
+    headers['authorization'] = token['token']
+    print(token['token'])
+
+    response = requests.post(F"{ENDPOINT}/api/room/getmembership", headers=headers, json=payload)
+    print(response.json())
+    assert response.status_code == 200
 
 def test_create_user_by_admin():
     payload = {
@@ -197,6 +211,13 @@ def test_get_chat_of_room():
 
     print(response.json())
     assert response.status_code == 200
+
+# def test_admin_logout():
+
+
+
+
+# def test_user_logout():
 
 
 
